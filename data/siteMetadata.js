b50295095c9f27a163b5cd68efe9ee0ccb81e079
etha39995 <etha39995@auburnschl.edu>
2026-06-05 10:55:03 -0400

    author, title, description

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'MySigmaBlog',
+  author: 'etha39995',
+  headerTitle: 'MySigmaBlog',
+  description: 'A blog created with technical prowess and creative flair',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
